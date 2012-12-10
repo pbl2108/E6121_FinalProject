d41c2924887206492de8ce1edc0bd826c6004c78
peter <pbl2108@columbia.edu>
2012-12-10 04:22:39 -0500

    Basic checker for detached handlers

--- Extension/src/content/browserOverlay.js
+++ Extension/src/content/browserOverlay.js
@@ -1,21 +1,54 @@
 /**
- * XULSchoolChrome namespace.
+ * VDomCheckerChrome namespace.
  */
 if ("undefined" == typeof(VDomCheckerChrome)) {
   var VDomCheckerChrome = {};
 };
 
+_loadHandler = function(event) {
+    VDomCheckerChrome.BrowserOverlay._onPageLoad(event);
+};
+ 
+gBrowser.addEventListener("load", _loadHandler, true);
+
 /**
  * Controls the browser overlay for the DOm Checker Extension extension.
  */
 VDomCheckerChrome.BrowserOverlay = {
-  /**
-   * Says 'Hello' to the user.
-   */
-  sayHello : function(aEvent) {
+  
+  _onPageLoad : function(event) {
+    // this is the content document of the loaded page.
+    let doc = event.originalTarget;
+    
+    if (doc instanceof HTMLDocument) {
+      // is this an inner frame?
+      if (doc.defaultView.frameElement) {
+        // Frame within a tab was loaded.
+        // Find the root document:
+        while (doc.defaultView.frameElement) {
+          doc = doc.defaultView.frameElement.ownerDocument;
+        }
+      }
+      
+      this._onDocumentLoad(doc);
+    }
+  },
+  
+  _onDocumentLoad : function(objDocument) {
+  },
+  
+  onMutation : function(mutation) {
+    alert(mutation.type);
+  },
+  
+  btnClickHandler : function(aEvent) {
     let stringBundle = document.getElementById("VDomChecker-string-bundle");
     let message = stringBundle.getString("VDomChecker.greeting.label");
     
-    window.alert(message);
+    let btnCheck = document.getElementById("btnCheck");
+    btnCheck.label = "Checking for Leaks...";
+    
+    let checker = new VDomCheckerChrome.HandlersChecker();
+    checker._createObserver();
   }
 };
